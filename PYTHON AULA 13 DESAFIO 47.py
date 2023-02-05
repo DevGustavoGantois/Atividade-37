7cb528c56a88e3b61fa17eaba0cf84dc828c5aed
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-02-05 20:22:44 -0300

    Add files via upload

---
+++ PYTHON AULA 13 DESAFIO 47.py
@@ -0,0 +1,14 @@
+print('                                          ATIVIDADE.                                    ')
+print('--------------------------------------------------------------------------------------------')
+print('Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.')
+print('---------------------------------------------------------------------------------------------------')
+print('Mostre todos os números pares de 0 a 50.')
+input('Aperte OK para começar:')
+print('Os números pares de 2 em 2 são...')
+print('ANALISANDO...')
+for c in range(0, 52, 2):
+    print(c)
+print('ACABOU.')
+
+
+
