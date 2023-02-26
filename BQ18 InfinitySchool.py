257ef55062e1221fb747a848a522a35f8466c212
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-02-26 01:44:37 -0300

    Add files via upload

---
+++ BQ18 InfinitySchool.py
@@ -0,0 +1,5 @@
+print('                                           ATIVIDADE                                 ')
+print(' ----------------------------------------------------------------------------------------')
+print('Desenvolva um programa que mostre uma contagem de 1 a 10 (Utilizando laços de repetição.)')
+for c in range (1, 11):
+    print(f'Contando --> {c}')
