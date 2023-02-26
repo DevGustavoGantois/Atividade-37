ad98b77571b8b44f3d7cde00abe083f14c7ab754
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-02-26 16:17:57 -0300

    Add files via upload

---
+++ BQ22 InfinitySchool.py
@@ -0,0 +1,9 @@
+print('                                     ATIVIDADE                             ')
+print('--------------------------------------------------------------------------------')
+print('Faça um programa que peça o número, e então, mostre na tela a tabuada desse número.')
+print('=='*40)
+numero = int(input('Digite um número:'))
+tabuada = numero * 1
+tabuada != 0
+for numero in range(1, 21,):
+    print(f'A tabuada desse número é {tabuada} x {numero} = {tabuada*numero}. ')
