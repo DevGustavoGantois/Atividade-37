e7c0fb70dcae9f39b1a7e3e3fcc489eb4fc6a3fb
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-02 12:44:59 -0300

    Add files via upload

---
+++ AULA14PYTHON DESAFIO 72.py
@@ -0,0 +1,16 @@
+print('==============================================ATIVIDADE===========================================================')
+print('Crie um programa que tenha uma Tupla totalmente preenchida com uma contagem por extenso, de zero até vinte. ')
+print('Seu programa deverá ler um programa pelo teclado (entre 0 e 20) e mostra-lá por extenso.')
+print('>>'*40)
+extenso = ('um' , 'dois' , 'tres' , 'quatro' , 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+while True:
+    count_number = int(input('Digite um número entre 0 e 20: '))
+    if count_number <= 0 and count_number <= 20:
+        break
+    print('Tente novamente. ', end = '')
+print(f'Você digitou o número {extenso[count_number]}')
+
+                        
+            
+        
+                
