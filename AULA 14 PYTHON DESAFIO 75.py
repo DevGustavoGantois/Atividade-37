7145d791ac7892a7cf06b927440db0b5b9e1da09
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2023-03-02 19:55:16 -0300

    Add files via upload

---
+++ AULA 14 PYTHON DESAFIO 75.py
@@ -0,0 +1,10 @@
+print('=============================================ATIVIDADE=========================================================')
+print('Desenvolva um programa que leia quatro valores pelo teclado e guarde-os em uma tupla. No final mostre:')
+
+print('A)Quantas vezes apareceu 9')
+
+print('B)Em que posição foi digitado o primeiro valor 3.')
+
+print('C)Quais foram os números pares.')
+print('=='*40)
+numero = (int(input('Digite um número:')), int(input('Digite outro número:')), int(input('Digite mais um número:')), int(input('Digite o último número:')))
