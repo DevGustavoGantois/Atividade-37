10b440cbd2d24224dffc1623e69ffcc602ec09bf
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-18 21:29:57 -0300

    Add files via upload

---
+++ atividade1listas.py
@@ -0,0 +1,11 @@
+#Crie uma lista com valores variados e envie a mesma para uma função que irá imprimir essa lista.
+
+def imprimir_lista(lista):
+    print("Lista:", lista)
+
+#Criar uma lista com valores variados.
+minha_lista = [11, 'DOIS', 3.0, True, 'cinco']
+
+#Chamar a função para imprimir a lista:
+imprimir_lista = (minha_lista)
+
---
+++ atividade2listas.py
@@ -0,0 +1,6 @@
+#Faça um Programa que leia uma lista de 5 números inteiros e mostre-os.
+
+list = [1,2,3,4,5]
+
+for list in range (0, len(list)):
+    print(f"{list + 1}")
