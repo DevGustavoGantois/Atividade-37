440b05ff0ac69176f7f308d26b77d7ba5b0e9db9
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-26 20:20:26 -0300

    Add files via upload

---
+++ AtividadeListasTuplas3.py
@@ -0,0 +1,7 @@
+#Defina uma lista com 5 itens que tenha, pelo menos, 3
+#classes diferentes. Utilize o print() para exibir o
+#terceiro elemento dessa lista.
+
+list_different_items = ['Gustavo', 12, 2.5, True, False]
+print(f"The third element of the list is ->{list_different_items[2]}")
+
