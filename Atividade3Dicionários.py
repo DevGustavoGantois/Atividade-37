ea5a755308da980f38071220f5a6b9873bce9b33
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-27 20:47:37 -0300

    Add files via upload

---
+++ Atividade3Dicionários.py
@@ -0,0 +1,15 @@
+#Crie um conjunto chamado frutas_vermelhas e adicione
+#as seguintes frutas a ele:
+#"
+#morango
+#"
+#,
+#"
+#cereja
+#"
+#e
+#"framboesa
+#". Em seguida, imprima o conjunto.
+
+red_fruits={'strawberry', 'cherry', 'raspberry'}
+print(red_fruits)
