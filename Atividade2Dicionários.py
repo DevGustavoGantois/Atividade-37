55eb8a52f8bb148fe0c34a1f5b0d6d3f05d8acd0
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-27 00:03:27 -0300

    Add files via upload

---
+++ Atividade2Dicionários.py
@@ -0,0 +1,9 @@
+#Verifique se a fruta
+#"banana"
+#está presente no conjunto
+#frutas e imprima o resultado.
+
+fruits = {"Apple", "Banana", "Orange", "Grape", "Strawberry", "Blueberry"}
+#Caso a fruta banana não estivesse no fruits o resultado seria False
+print('Banana' in fruits)
+#Irá retornar True.
