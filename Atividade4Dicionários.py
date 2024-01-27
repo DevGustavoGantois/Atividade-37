90c35bd40ef2275946feab98907447740b1412a2
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-27 20:52:22 -0300

    Add files via upload

---
+++ Atividade4Dicionários.py
@@ -0,0 +1,12 @@
+#Remova a fruta
+#"cereja"
+#do conjunto frutas_vermelhas e
+#imprima o conjunto atualizado.
+
+
+#Dicionário com os tipos de frutas:
+red_fruits={"strawberry", 'Cherry', 'Apple', 'Watermelon'}
+#Removendo  a fruta cereja com a função remove.
+red_fruits.remove('Cherry')
+#Mostrando o novo dicionário na tela:
+print(red_fruits)
