ccea3ee74517fe8960c16d1b30a9f724b2c19173
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-26 23:10:30 -0300

    Add files via upload

---
+++ Atividade1Dicionários.py
@@ -0,0 +1,27 @@
+#Crie um conjunto vazio chamado frutas e adicione as
+#seguintes frutas a ele:
+#"
+#maçã"
+#,
+#"banana
+#"
+#,
+#"
+#uva
+#"
+#,
+#"laranja
+#"
+#e
+#"
+#morango
+#". Em seguida, imprima o conjunto.
+
+fruits = set()
+
+fruits.add("Apple")
+fruits.add("Banana")
+fruits.add("grape")
+fruits.add("Orange")
+fruits.add("strawberry")
+print(fruits)
