52b631306cca1949389b2c0f3ef9d59633eef9d8
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-26 21:00:08 -0300

    Add files via upload

---
+++ AtividadeListasTuplas4.py
@@ -0,0 +1,9 @@
+#Crie uma tupla para representar as informações de três
+#palestrantes, cada uma contendo o nome, o tema da
+#palestra e a instituição à qual estão vinculados.
+
+lecture1=('Gustavo' , 'lecture of Python', 'Infinity School')
+lecture2=('Augusto' , 'Lecture of JAVA', 'Cubos Academy')
+lecture3=('Nayra', 'Lecture of JavaScript', 'Infinity School')
+
+print(f"The informations of third participant -> {lecture3}")
