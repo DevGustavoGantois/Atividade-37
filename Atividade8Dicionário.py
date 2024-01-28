44e17528f124d9048bf83046b391a01f7c54f2b0
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-28 19:23:02 -0300

    Add files via upload

---
+++ Atividade8Dicionário.py
@@ -0,0 +1,33 @@
+#Escreva um programa que percorra um dicionário
+#contendo informações de pessoas (nome, idade) e
+#exiba essas informações.
+
+information_people={
+    "Name 1": "Gustavo",
+    "Age 1": "20",
+    "Name 2": "Murilo",
+    "Age 2": "22",
+    "Name 3": "Jose",
+    "Age 3": "89"
+}
+
+for data in information_people:
+    print(information_people[data])
+
+
+#information_people = {
+#"Name 1": "Gustavo",
+#"Age 1": "20",
+#"Name 2": "Murilo",
+#"Age 2": "22",
+#"Name 3": "Jose",
+#"Age 3": "89
+
+#}
+    
+#Itera sobre as chaves do dicionário:
+
+#for key in information_people:
+    #Exibe as informações em pares chave-valor
+    #print(f"{key}: {information_people[key]}")
+
