8599e7cb0a1ce9408793840c46bfee378f5f304f
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-01-29 14:05:03 -0300

    Add files via upload

--- Atividade11Dicionários.py
+++ Atividade11Dicionários.py
@@ -3,26 +3,28 @@
 #valor ao dicionário, atualizando o valor se a chave já
 #existir.
 def receive_dictionary(key, value, dictionary):
-    #Verifica se a chave já existe no dicionário
+    # Verifica se a chave já existe no dicionário
     if key in dictionary:
-        print(f"The key in dictionary {key} exists. Updating the {value}")
+        print(f"The key in dictionary {key} exists. Updating the value to {value}")
     else:
         dictionary[key] = value
-#Dicionário criado
-my_dictionary = {'Gustavo': 20, 'Matheus': 22, 'Leandro': 25}
-#Recebe entrada do usuário para chave e valor
-key_user = input('Enter the key:')
-value_user = input('Enter the value:')
 
-#Coverte o valor para um tipo adequado(por exemplo, int)
+# Dicionário criado
+my_dictionary = {'Gustavo': 20, 'Matheus': 22, 'Leandro': 25}
+
+# Recebe entrada do usuário para chave e valor
+key_user = input('Enter the key: ')
+value_user = input('Enter the value: ')
+
+# Converte o valor para um tipo adequado (por exemplo, int)
 try:
     value_user = int(value_user)
 except ValueError:
     print("Error: Value must be an integer")
     exit()
 
-#Chamando a função para adicionar ou atualizar a entrada no dicionário    
-receive_dictionary(my_dictionary, key_user, value_user)
+# Chamando a função para adicionar ou atualizar a entrada no dicionário    
+receive_dictionary(key_user, value_user, my_dictionary)
 
-#Exibe o dicinário atualizado
-print('Update Dictionary:', my_dictionary)
+# Exibe o dicionário atualizado
+print('Updated Dictionary:', my_dictionary)
