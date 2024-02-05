7963d6eeb61679b407cedc2beeca86bacb271f9a
DevGustavoGantois <123424700+DevGustavoGantois@users.noreply.github.com>
2024-02-04 22:30:46 -0300

    Add files via upload

---
+++ Atividade1Funções.py
@@ -0,0 +1,12 @@
+#Crie uma função que receba um nome e imprima uma saudação personalizada.
+
+def print_salutation(salutation):
+    #Essa função irá retornar a saudação 'Hello world'.
+    
+    print(f'Hello {salutation}')
+
+salutation = 'World'
+
+print_salutation(salutation)
+
+
